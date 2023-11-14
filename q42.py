eaf01173c6533eb91281cd204ffa36bdc5d6c80d
arllenfelipe <147093058+arllenfelipe@users.noreply.github.com>
2023-11-14 16:05:02 -0300

    Add files via upload

---
+++ q42.py
@@ -0,0 +1,12 @@
+# NOTA: NÃO SEI SE ENTENDI BEM O QUE FOI PEDIDO, PORTANTO, PODE ESTAR ERRADO.
+
+# Definição Variavis:
+a = int(input("Variavel A: "))
+b = int(input("Variavel B: "))
+c = int(input("Variavel C: "))
+d = int(input("Variavel D: "))
+
+resultado = a > b and c or not d
+
+# Definição Codigos:
+print(resultado)
